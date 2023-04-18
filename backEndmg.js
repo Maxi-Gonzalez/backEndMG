e36630ed94ef800a8011fd1368cbed19444b9d93
MaximilianoGonzalez <maxi.gonzalez096@gmail.com>
2023-04-18 14:48:38 -0300

    desafio 1

--- backEndmg.js
+++ backEndmg.js
@@ -10,10 +10,10 @@ class ProductManager {
   addProduct = (title, description, price, thumbnail, code, stock) => {
     let codExist = this.products.find((product) => product.code == code);
 
-    if (codeExist) {
+    if (codExist) {
       console.log("Codigo ya existente, debe ingresar otro");
     } else {
-      return this.prod.push({
+      return this.products.push({
         title: title,
         description: description,
         price: price,
@@ -24,5 +24,15 @@ class ProductManager {
     }
   };
 
-  getProductById = () => {};
+  getProductById = (code) => {
+    let codExist = this.products.find((product) => product.code == code);
+
+    if (codExist) {
+      return codExist;
+    } else {
+      return "not found";
+    }
+  };
 }
+
+
