c80d1e1aad82790e42a8086a9afc33f03fd12c2a
MaximilianoGonzalez <maxi.gonzalez096@gmail.com>
2023-04-27 18:50:34 -0300

    agregando funcion update y delete

--- backEnd/backEndmg.js
+++ backEnd/backEndmg.js
@@ -1,22 +1,20 @@
-const fs = require(`fs`)
-let readFile =fs.readFileSync(`backEnd/text.txt`, {encoding:`utf-8`})
+const fs = require(`fs`);
+let readFile = fs.readFileSync(`backEnd/products.json`, { encoding: `utf-8` });
 
-console.log(readFile)
-
+console.log(readFile);
 
 class ProductManager {
   constructor() {
-    this.path = "./products.json"
+    this.path = "backEnd/products.json";
     this.products = [];
   }
 
   async getProducts() {
     try {
-      const data =  fs.readFile(this.path);
+      const data = fs.readFile(this.path);
       this.products = JSON.parse(data);
     } catch (err) {
       if (err.code === "ENOENT") {
-        // Si el archivo no existe, no hay productos todavía
         this.products = [];
       } else {
         throw err;
@@ -28,37 +26,39 @@ class ProductManager {
   addProduct = (title, description, price, thumbnail, code, stock) => {
     let codExist = this.products.find((product) => product.code == code);
 
-  if (codExist) {
-    let x = Math.floor(Math.random() * 100);
-    let id = code + x;
-    console.log(
-      `Codigo ya existente, le asignaremos uno no utilizado y el codigo es ${id}`
-    );
+    if (codExist) {
+      let x = Math.floor(Math.random() * 100);
+      let id = code + x;
+      console.log(
+        `Codigo ya existente, le asignaremos uno no utilizado y el codigo es ${id}`
+      );
 
-     this.products.push({
-      title: title,
-      description: description,
-      price: price,
-      thumbnail: thumbnail,
-      code: id,
-      stock: stock,
-    });
-     fs.writeFileSync(`backEnd/products.json`, JSON.stringify(this.products),{encoding:`utf-8`})
-  } else {
-     this.products.push({
-      title: title,
-      description: description,
-      price: price,
-      thumbnail: thumbnail,
-      code: code,
-      stock: stock,
-    });
-    fs.writeFileSync(`backEnd/products.json`, JSON.stringify(this.products),{encoding:`utf-8`})
- 
-  }
-  
-}
-getProductById = (code) => {
+      this.products.push({
+        title: title,
+        description: description,
+        price: price,
+        thumbnail: thumbnail,
+        code: id,
+        stock: stock,
+      });
+      fs.writeFileSync(`backEnd/products.json`, JSON.stringify(this.products), {
+        encoding: `utf-8`,
+      });
+    } else {
+      this.products.push({
+        title: title,
+        description: description,
+        price: price,
+        thumbnail: thumbnail,
+        code: code,
+        stock: stock,
+      });
+      fs.writeFileSync(`backEnd/products.json`, JSON.stringify(this.products), {
+        encoding: `utf-8`,
+      });
+    }
+  };
+  getProductById = (code) => {
     let codExist = this.products.find((product) => product.code == code);
 
     if (codExist) {
@@ -70,35 +70,36 @@ getProductById = (code) => {
 
   async updateProduct(id, update) {
     await this.getProducts();
-    const productExist = this.products.findIndex((product) => product.id === id);
+    const productExist = this.products.findIndex(
+      (product) => product.id === id
+    );
     if (productExist === -1) {
       console.log(`No existe producto con el id ${id}`);
     }
-    const updatedProduct = Object.assign({}, this.products[productExist], update);
+    const updatedProduct = Object.assign(
+      {},
+      this.products[productExist],
+      update
+    );
     this.products[productExist] = updatedProduct;
-    await fs.writeFile("./products.json", JSON.stringify(this.products),{encoding:`utf-8`});
+    await fs.writeFile("backEnd/products.json", JSON.stringify(this.products), {
+      encoding: `utf-8`,
+    });
   }
 
   async deleteProduct(id) {
     await this.getProducts();
-    const productIndex = this.products.findIndex((product) => product.id === id);
+    const productIndex = this.products.findIndex(
+      (product) => product.id === id
+    );
     if (productIndex === -1) {
       console.log(`Product with id ${id} not found`);
     }
     this.products.splice(productIndex, 1);
-    await fs.writeFile("./products.json", JSON.stringify(this.products),{encoding:`utf-8`});
+    await fs.writeFile("backEnd/products.json", JSON.stringify(this.products), {
+      encoding: `utf-8`,
+    });
   }
 }
 
 
-
-
-
-
-
-let producto = new ProductManager()
-
-producto.addProduct('Pollo','Ave',100,'Sin imagen','a1',20)
-producto.addProduct('gallina','Ave',100,'Sin imagen','a13',20)
-
-console.log(producto.getProductById("a13"))
